b19613f3ddffa98b796716f7f3aa8b3ce2d435b9
Imane <belkessa.imanebelkes@gmail.com>
2025-03-31 13:26:00 +0200

    test

---
+++ lib/list.py
@@ -0,0 +1,61 @@
+import pandas as pd
+import random
+
+class Dataprep:
+    full_data = [
+        {
+            "id": 1,
+            "email": "george.bluth@reqres.in",
+            "first_name": "George",
+            "last_name": "Bluth",
+            "avatar": "https://reqres.in/img/faces/1-image.jpg"
+        },
+        {
+            "id": 2,
+            "email": "janet.weaver@reqres.in",
+            "first_name": "Janet",
+            "last_name": "Weaver",
+            "avatar": "https://reqres.in/img/faces/2-image.jpg"
+        },
+        {
+            "id": 3,
+            "email": "emma.wong@reqres.in",
+            "first_name": "Emma",
+            "last_name": "Wong",
+            "avatar": "https://reqres.in/img/faces/3-image.jpg"
+        },
+        {
+            "id": 4,
+            "email": "eve.holt@reqres.in",
+            "first_name": "Eve",
+            "last_name": "Holt",
+            "avatar": "https://reqres.in/img/faces/4-image.jpg"
+        },
+        {
+            "id": 5,
+            "email": "charles.morris@reqres.in",
+            "first_name": "Charles",
+            "last_name": "Morris",
+            "avatar": "https://reqres.in/img/faces/5-image.jpg"
+        },
+        {
+            "id": 6,
+            "email": "tracey.ramos@reqres.in",
+            "first_name": "Tracey",
+            "last_name": "Ramos",
+            "avatar": "https://reqres.in/img/faces/6-image.jpg"
+        }
+    ]
+
+    def __init__(self, path):
+        self.path = path
+    
+    def CreatData(self):
+        data = pd.DataFrame(Dataprep.full_data).sample(frac=0.5, random_state=random.randint(1, 100))
+        data.to_csv(self.path, index=False) 
+        
+    
+        
+if __name__ == "__main__":
+    data_prep = Dataprep("data/data.csv")  # Provide the filename
+    data_prep.CreatData()
